1f0bd614c4440df811fd28cf497825773b33f167
Muhammad Afaq <mafaq3615@gmail.com>
2023-12-05 09:41:54 +0500

    added PR message in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ const Header = () => {
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="NetFlix Logo"
       />
+      <p>Lets create a PR for testing in testing branch</p>
     </div>
   );
 };
